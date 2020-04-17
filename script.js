7cce1488b9004a3408a63982828c575097a9773d
Andrea Bentley <andrea@bentleytech.com>
2020-04-17 13:33:39 -0400

    updating navbar to work on mobile

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
 
 	// Materialize animation code for front - end
 	// M.AutoInit(); causes runtime error - use individual inits below
-	$('.sidenav').sidenav();
+	$('.sidenav').sidenav({edge: "right"});
 	$('.modal').modal();
 	$('select').formSelect();
 });
@@ -42,13 +42,19 @@ function initializeParkData() {
 		const parkHtml2 = ">";
 		const parkHtml2alt = " selected>";
 		const parkHtml3 = "</option>";
+		const sideHtml1 = '<li><a value='
+		const sideHtml2 = '>';
+		const sideHtml3 = '</a></li>';
+
 
 		if (index === 0) {
 			$("#parksChooser").append(parkHtml1 + index + parkHtml2alt + name + parkHtml3);
-			$("#parksChooser2").append(parkHtml1 + index + parkHtml2alt + name + parkHtml3);
+			$("#mobile-demo").append(sideHtml1 + index + sideHtml2 + name + sideHtml3);
+			// $("#parksChooser2").append(parkHtml1 + index + parkHtml2alt + name + parkHtml3);
 		} else {
 			$("#parksChooser").append(parkHtml1 + index + parkHtml2 + name + parkHtml3);
-			$("#parksChooser2").append(parkHtml1 + index + parkHtml2 + name + parkHtml3);
+			$("#mobile-demo").append(sideHtml1 + index + sideHtml2 + name + sideHtml3);
+			// $("#parksChooser2").append(parkHtml1 + index + parkHtml2 + name + parkHtml3);
 		}
 	}
 
